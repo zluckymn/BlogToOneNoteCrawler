9311be21686829ea2e549ebd6c4d0fd82591626a
MN <z.luckymn@gmail.com>
2020-09-09 11:29:14 +0800

    Update PeiseNetCrawler.cs

--- SimpleCrawler-master/demo/peiseNet/PeiseNetCrawler.cs
+++ SimpleCrawler-master/demo/peiseNet/PeiseNetCrawler.cs
@@ -31,7 +31,7 @@ namespace SimpleCrawler.Demo
     public class PeiseNetCrawler : ISimpleCrawler
     {
 
-       // private   string connStr = "mongodb://MZsa:MZdba@59.61.72.34:37088/SimpleCrawler";
+      
         DataOperation dataop = null;
         private CrawlSettings Settings = null;
       
